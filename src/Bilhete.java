e7a8568e39bc38932db91a783e56a192e91bb206
matheus <matheusmgs17@gmail.com>
2023-04-01 12:19:01 -0300

    Classe Bilhete e Compra - Implementacao do constru

--- src/Bilhete.java
+++ src/Bilhete.java
@@ -1,3 +1,80 @@
-public class Bilhete {
+public class Bilhete { // matheus
     
+    private String user;
+    private String cpf;
+    private String sala;
+    private String sessao;
+    private String filme;
+    private double valor;
+    private int cadeiraComprada;
+
+
+// construtor da classe Bilhete;
+public Bilhete(String user, String cpf, String sala, String sessao, String filme, 
+double valor,int cadeiraComprada) {
+    this.user = user;
+    this.cpf = cpf;
+    this.sala = sala;
+    this.sessao = sessao;
+    this.filme = filme;
+    this.valor = valor;
+    this.cadeiraComprada = cadeiraComprada;
+}
+    
+// Getters and setters dos atributos;
+public String getUser() {
+    return user;
+}
+
+public void setUser(String user) {
+    this.user = user;
+}
+public String getCpf() {
+    return cpf;
+}
+
+public void setCpf(String cpf) {
+    this.cpf = cpf;
+}
+    
+public String getSala() {
+    return sala;
+}
+
+public void setSala(String sala) {
+    this.sala = sala;
+}
+
+public String getSessao() {
+    return sessao;
+}
+
+public void setSessao(String sessao) {
+    this.sessao = sessao;
+}
+
+public String getFilme() {
+    return filme;
+}
+
+public void setFilme(String filme) {
+    this.filme = filme;
+}
+
+public double getValor() {
+    return valor;
+}
+
+public void setValor(double valor) {
+    this.valor = valor;
+} 
+    
+public int getCadeiraComprada() {
+    return cadeiraComprada;
+}
+
+public void setCadeiraComprada(int cadeiraComprada) {
+    this.cadeiraComprada = cadeiraComprada;
+}
+
 }
--- src/Compra.java
+++ src/Compra.java
@@ -1,3 +1,55 @@
-public class Compra {
+public class Compra { // matheus
+
+    /*  A classe compra é a classe que contém as compras que podem ser realizadas, a
+    compra pode conter itens como pipoca, refrigerante, chocolate ou qualquer outro
+    item que esteja disponível(para este projeto não vamos trabalhar com estoque de
+    itens, suponha que o estoque para os itens vendidos seja infinito), e deve ter um ou
+    mais bilhetes. 
+
+        O método de compra de bilhetes deve ser um método a parte,
+    inicialmente o usuário irá buscar pelas sessões em cada sala, cada sala deve conter
+    o filme em cartaz juntamente com o horário do filme(para este projeto, não vamos
+    levar em consideração a comparação do horário atual com o horário de exibição do
+    filme), uma vez que a sessão da sala tenha sido escolhida o usuário pode comprar
+    os bilhetes, caso ele compre apenas um bilhete ele vai escolher uma poltrona livre
+    dentro da sessão, o usuário também pode comprar vários bilhetes, caso seja
+    comprado o sistema dará sugestões de cadeiras que estejam juntas para que todos
+    possam se sentar juntos. */
+
+    private int pipoca; 
+    private int chocolate;
+    private int refrigerante;
+
+
+public Compra(int pipoca, int refrigerante, int chocolate) {
+    this.pipoca = pipoca;
+    this.refrigerante = refrigerante;
+    this.chocolate = chocolate;
+}
     
+
+public int getPipoca() {
+    return pipoca;
+}
+
+public void setPipoca(int pipoca) {
+    this.pipoca = pipoca;
+}
+    
+public int getRefrigerante() {
+    return refrigerante;
+}
+
+public void setRefrigerante(int refrigerante) {
+    this.refrigerante = refrigerante;
+}
+    
+public int getChocolate() {
+    return chocolate;
+}
+
+public void setChocolate(int chocolate) {
+    this.chocolate = chocolate;
+}
+
 }
