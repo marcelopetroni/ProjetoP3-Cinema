7e92db446b35ce7a584cbff533aa471e7d7e0b7f
Marcelo Petroni <marceloapetroni@hotmail.com>
2023-04-01 15:24:43 -0300

    Classe Filme e Sala - Modificações iniciais

--- src/Filme.java
+++ src/Filme.java
@@ -1,3 +1,37 @@
 public class Filme {
+    private String nome;
+    private int duração;
+    private String sinopse;
+    private double valor;
     
+    public Filme(String nome, int duração, String sinopse, double valor) {
+        this.nome = nome;
+        this.duração = duração;
+        this.sinopse = sinopse;
+        this.valor = valor;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getDuração() {
+        return duração;
+    }
+    public void setDuração(int duração) {
+        this.duração = duração;
+    }
+    public String getSinopse() {
+        return sinopse;
+    }
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 }
--- src/Sala.java
+++ src/Sala.java
@@ -1,3 +1,23 @@
 public class Sala {
-    
+    private Sessao[] horarios = new Sessao[7];;
+
+    public Sala(Sessao[] horarios) {
+        this.horarios = horarios;
+    }
+
+    public Sessao[] getHorarios() {
+        return horarios;
+    }
+
+    public void setHorarios(Sessao[] horarios) {
+        this.horarios = horarios;
+    }
+    public void sessoesDisponiveis() {
+        System.out.println("Sessões Disponíveis: ");
+        for(int i = 0; i < horarios.length; i++) {
+            if(horarios[i] != null) {
+                System.out.println(horarios.GetFilme().GetNome());
+            }
+        }
+    }
 }
