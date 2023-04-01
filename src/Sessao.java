9adb9b39e8e2e7480fe53fe976c5bf61a14259c7
Pedro Bitu <99653881+Pedrobitu@users.noreply.github.com>
2023-04-01 19:36:37 -0300

    Atualização Sessão

--- src/Sessao.java
+++ src/Sessao.java
@@ -19,23 +19,43 @@ public class Sessao {
     }
 
     //métodos
-    public void DisponivelParaCompra() {
+    public boolean disponivelParaCompra(int a, int b) {
 
         for(int i = 0; i < 10; i++){
-            for(int j = 0; j < 15; j++){
-                if(cadeiras[i][j] == 0){
-                    System.out.println(cadeiras[i][j] + "disponivel");
+            for(int j = 0; j < 15; j++){    
+                if(a == i && b == j){
+                    if(cadeiras[i][j] == 0){
+                        System.out.println(cadeiras[i][j] + "disponivel");
+                        return true;
+                    }
                 }
             }
         }
+        return false;
     }
 
-    public void AlternarDisponibilidade(int i, int j) {
+    public void alternarDisponibilidade(int i, int j) {
 
         if(cadeiras[i][j] == 0){
             cadeiras[i][j] = 1;
         }else if(cadeiras[i][j] == 1){
             cadeiras[i][j] = 0;
+        }
+    }
+
+    public void numeroCadeira(int k){
+
+        int a = 1;
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 15; j++){
+                cadeiras[i][j] = a;
+                if(cadeiras[i][j] == k){
+                    if(disponivelParaCompra(i, j) == true){
+                        System.out.println("cadeira " + cadeiras[i][j] + "numero " + a);
+                    }
+                }
+                a++;
+            }
         }
     }
 }
