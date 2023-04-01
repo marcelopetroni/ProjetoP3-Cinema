085e5226306b12683594b202dc741c5ad331e21e
Pedro Bitu <99653881+Pedrobitu@users.noreply.github.com>
2023-04-01 17:44:20 -0300

    continuaçao sessao

--- src/Sessao.java
+++ src/Sessao.java
@@ -1,7 +1,23 @@
 public class Sessao {
 
     private int[][] cadeiras = new int[10][15];
-    
+    private String filme;
+
+    //gets & sets
+    public String getFilme() {
+        return filme;
+    }
+    public void setFilme(String filme) {
+        this.filme = filme;
+    }
+
+    //construtor
+    public Sessao(String filme, int[][] cadeiras){
+
+        this.filme = filme;
+        this.cadeiras = cadeiras;
+    }
+
     //métodos
     public void DisponivelParaCompra() {
 
@@ -11,6 +27,15 @@ public class Sessao {
                     System.out.println(cadeiras[i][j] + "disponivel");
                 }
             }
+        }
+    }
+
+    public void AlternarDisponibilidade(int i, int j) {
+
+        if(cadeiras[i][j] == 0){
+            cadeiras[i][j] = 1;
+        }else if(cadeiras[i][j] == 1){
+            cadeiras[i][j] = 0;
         }
     }
 }
